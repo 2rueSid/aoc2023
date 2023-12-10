a82dd7f54cfd2a533b22b2d5ab14bbaa45032f71
Dima Kachurovskyy <mekaa2.718@gmail.com>
2023-12-10 10:26:30 -0500

    day2 pt2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -20,13 +20,16 @@ fn task1(games: &Vec<String>) -> u32 {
         game_count += 1;
         let mut passed = true;
         for cap in get_color.captures_iter(game) {
-            if &cap[2] == red.0 && &cap[1].parse::<i32>().expect("Number") > &red.1 {
+            let color = &cap[2];
+            let number = &cap[1].parse::<i32>().expect("Number");
+
+            if color == red.0 && number > &red.1 {
                 passed = false;
                 break;
-            } else if &cap[2] == green.0 && &cap[1].parse::<i32>().expect("Number") > &green.1 {
+            } else if color == green.0 && number > &green.1 {
                 passed = false;
                 break;
-            } else if &cap[2] == blue.0 && &cap[1].parse::<i32>().expect("Number") > &blue.1 {
+            } else if color == blue.0 && number > &blue.1 {
                 passed = false;
                 break;
             }
@@ -40,6 +43,39 @@ fn task1(games: &Vec<String>) -> u32 {
     res
 }
 
+fn task2(games: &Vec<String>) -> u32 {
+    let red = "red";
+    let green = "green";
+    let blue = "blue";
+
+    let get_color = Regex::new(r"(\d+)\s*(blue|red|green)").unwrap();
+
+    let mut res: u32 = 0;
+
+    for game in games {
+        let mut max_red: u32 = 1;
+        let mut max_green: u32 = 1;
+        let mut max_blue: u32 = 1;
+
+        for cap in get_color.captures_iter(game) {
+            let color = &cap[2];
+            let number = cap[1].parse::<u32>().expect("Number");
+
+            if color == red && number > max_red {
+                max_red = number;
+            } else if color == green && number > max_green {
+                max_green = number;
+            } else if color == blue && number > max_blue {
+                max_blue = number;
+            }
+        }
+
+        res += max_red * max_blue * max_green;
+    }
+
+    res
+}
+
 fn main() {
     let file = File::open(FILEPATH).expect(format!("File does not exists {FILEPATH}").as_str());
     let reader = BufReader::new(file);
@@ -48,4 +84,7 @@ fn main() {
 
     let res1 = task1(&games);
     println!("RESULT FOR TASK 1 -> {res1}");
+
+    let res2 = task2(&games);
+    println!("RESULT FOR TASK 2 -> {res2}");
 }
